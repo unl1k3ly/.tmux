1d32002a61e8cb1bf50c878100fa88c3602328bc
unl1k3ly <vinicius777@gmail.com>
2018-03-14 18:30:45 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+echo "Installing . . . "
+sleep 2
+
+## Installing main 
+cd
+git clone https://github.com/unl1k3ly/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+## Installing TMP
+mkdir -p ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+############################################################################################
+cat <<EOT >>~/.tmux/.tmux.conf.local
+
+##################  TPM  ##############################
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# Other examples:
+# set -g @plugin 'github_username/plugin_name'
+# set -g @plugin 'git@github.com/user/plugin'
+# set -g @plugin 'git@bitbucket.com/user/plugin'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
+##################  TPM  ##############################
+
+EOT
+###############################################################################################
+
+# Install tmux-resurrect throught TMP
+echo "set -g @plugin 'tmux-plugins/tmux-resurrect'" >> ~/.tmux/.tmux.conf.local
+
+echo "All done!"
+
+
